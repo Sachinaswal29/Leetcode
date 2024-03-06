8c692930c5c02630c5e44d0327e78a833bfc90ee
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-03-06 17:32:28 +0530

    Time: 338 ms (32.52%), Space: 17.2 MB (46.09%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[201][201];
+    int solve(vector<vector<int>>& grid, int row, int col){
+        if(row==grid.size()-1) return dp[row][col]=grid[row][col];
+        if(dp[row][col]!=-1) return dp[row][col];
+        int mini = INT_MAX;
+        for(int i=0 ; i<grid.size() ; i++){
+            if(i!=col){
+                mini = min(mini , grid[row][col] + solve(grid,row+1,i));
+            }
+        }
+        return dp[row][col]=mini;
+    }
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int ans=INT_MAX;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<grid.size();i++){
+            ans=min(ans,solve(grid,0,i));
+        }
+        return ans;
+    }
+};
