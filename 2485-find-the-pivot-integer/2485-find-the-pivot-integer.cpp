b41820d8c85a27c0089aa6a998f49249a8a9efa6
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-03-13 10:05:17 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (41.86%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        for(int i=1;i<=n;i++){
+            int leftsum = (i*(i+1))/2;
+            int rightsum = (n*(n+1))/2 - leftsum + i;
+            if(leftsum==rightsum) return i;
+        }
+        return -1;
+    }
+};
