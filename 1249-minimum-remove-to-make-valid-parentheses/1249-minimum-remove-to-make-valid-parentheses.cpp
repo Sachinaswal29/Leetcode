229f5d4461bbde172e8f535446fcfb47c35b622b
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-06 12:25:17 +0530

    Time: 25 ms (31.31%), Space: 13.5 MB (26.27%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,25 +1,74 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int>st;
-        unordered_set<int>remove_idx;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(') st.push(i);
-            else if(s[i]==')'){
-                if(st.empty()) remove_idx.insert(i);
-                else st.pop();
-            }
-        } 
+        /*--------------------------APPROACH 1-----------------------*/
+        // stack<int>st;
+        // unordered_set<int>remove_idx;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='(') st.push(i);
+        //     else if(s[i]==')'){
+        //         if(st.empty()) remove_idx.insert(i);
+        //         else st.pop();
+        //     }
+        // } 
         
-        while(!st.empty()){
-            remove_idx.insert(st.top());
-            st.pop();
-        }
+        // while(!st.empty()){
+        //     remove_idx.insert(st.top());
+        //     st.pop();
+        // }
 
-        string ans="";
+        // string ans="";
+        // for(int i=0;i<s.size();i++){
+        //     if(remove_idx.find(i)==remove_idx.end()) ans.push_back(s[i]);
+        // }
+        // return ans;
+
+    /*------------------------------APPROACH 2-------------------------*/
+        // stack<int>st;
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]=='(') st.push(i);
+        //     else if(s[i]==')'){
+        //         if(st.empty()) s[i]='#';
+        //         else st.pop();
+        //     }
+        // }
+        // while(st.size()){
+        //     s[st.top()]='#';
+        //     st.pop();
+        // }
+        // s.erase(remove(s.begin(), s.end(), '#'), s.end()); 
+        // return s;
+
+    /*------------------------------APPROACH 3-------------------------*/
+        string temp="";
+        int open=0;
         for(int i=0;i<s.size();i++){
-            if(remove_idx.find(i)==remove_idx.end()) ans.push_back(s[i]);
+            if(s[i]=='(') {
+                open++;
+                temp.push_back(s[i]);
+            }
+            else if(s[i]==')' && open > 0){
+                open--;
+                temp.push_back(s[i]);
+            }
+            else if(s[i]>='a' && s[i]<='z') temp.push_back(s[i]);
         }
+        
+        string ans="";
+        int close=0;
+        for(int i=temp.size()-1;i>=0;i--){
+            if(temp[i]==')') {
+                close++;
+                ans.push_back(temp[i]);
+            }
+            else if(temp[i]=='(' && close > 0){
+                close--;
+                ans.push_back(temp[i]);
+            }
+            else if(temp[i]>='a' && temp[i]<='z') ans.push_back(temp[i]);
+        }
+        
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
