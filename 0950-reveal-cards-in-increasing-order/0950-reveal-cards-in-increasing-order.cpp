7a8552c4f6842e1db983f691fc7196bf139b41a7
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-10 10:32:20 +0530

    Time: 0 ms (100.00%), Space: 10.9 MB (34.66%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        
+        // using queue
         int n=deck.size();
+        queue<int>q;
+        for(int i=0;i<n;i++) q.push(i);
         sort(deck.begin(),deck.end());
-        vector<int>ans(n,0);
-        int i=0,j=0;
-        bool skip=false;
-        while(i<n){
-            if(ans[j]==0){
-                if(!skip){
-                    ans[j]=deck[i];
-                    i++;
-                }
-                skip=!skip;
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[q.front()]=deck[i];
+            q.pop();
+            if(!q.empty()){
+                q.push(q.front());
+                q.pop();
             }
-            j=(j+1)%n;
         }
         return ans;
+            
+        // int n=deck.size();
+        // sort(deck.begin(),deck.end());
+        // vector<int>ans(n,0);
+        // int i=0,j=0;
+        // bool skip=false;
+        // while(i<n){
+        //     if(ans[j]==0){
+        //         if(!skip){
+        //             ans[j]=deck[i];
+        //             i++;
+        //         }
+        //         skip=!skip;
+        //     }
+        //     j=(j+1)%n;
+        // }
+        // return ans;
     }
 };
