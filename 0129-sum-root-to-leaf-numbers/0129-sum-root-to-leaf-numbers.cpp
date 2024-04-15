8cef84417f725ab9895d9f7bd17e86019c830415
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-15 12:28:35 +0530

    Time: 5 ms (26.49%), Space: 11.3 MB (23.84%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,15 +11,33 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root, int sum){
-        if(!root) return 0;
-        sum=sum*10+root->val;
-        if(!root->left and !root->right){
-            return sum;
-        }
-        return solve(root->left,sum) + solve(root->right,sum);
-    }
+    // Using DFS
+    // int solve(TreeNode* root, int sum){
+    //     if(!root) return 0;
+    //     sum=sum*10+root->val;
+    //     if(!root->left and !root->right){
+    //         return sum;
+    //     }
+    //     return solve(root->left,sum) + solve(root->right,sum);
+    // }
+    // int sumNumbers(TreeNode* root) {
+    //     return solve(root,0);
+    // }
+
+    // Using BFS
     int sumNumbers(TreeNode* root) {
-        return solve(root,0);
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        int totalSum=0;
+        while(!q.empty()){
+            TreeNode* frontNode=q.front().first;
+            int curSum=q.front().second;
+            q.pop();
+            curSum=curSum*10+frontNode->val;
+            if(!frontNode->left and !frontNode->right) totalSum+=curSum;
+            if(frontNode->left) q.push({frontNode->left,curSum});
+            if(frontNode->right) q.push({frontNode->right,curSum});
+        }
+        return totalSum;
     }
 };
