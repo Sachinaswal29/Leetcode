52552dd1bef619ec2518b7b9942635d0c91d8c9e
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-17 11:49:23 +0530

    Time: 8 ms (65.38%), Space: 18.1 MB (95.14%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    // Using DFS
+    // Using DFS   TC-> O(N2)
     // string ans;
     // void solve(TreeNode* root, string curr){
     //     if(!root) return;
@@ -28,22 +28,45 @@ public:
     //     return ans;
     // }
 
-    // Using BFS
-    string smallestFromLeaf(TreeNode* root) {
-       string ans="";
-       queue<pair<TreeNode*,string>>q;
-       q.push({root,""});
-       while(!q.empty()){
-            TreeNode* frontNode=q.front().first;
-            string curr=q.front().second;
-            q.pop();
-            curr=char(frontNode->val+'a')+curr;
-            if(frontNode->left==NULL && frontNode->right==NULL){
-                if(ans=="" || curr<ans) ans=curr;
-            }
-            if(frontNode->left) q.push({frontNode->left,curr});
-            if(frontNode->right) q.push({frontNode->right,curr});
-       }
-       return ans;
+
+    // TC-> O(NlogN)
+    string ans;
+    void solve(TreeNode* root, string& curr) {
+        if (!root) return;
+        curr.push_back(root->val+'a');
+        if (!root->left && !root->right) {
+            reverse(curr.begin(), curr.end());
+            if (ans.empty() || curr < ans) ans = curr;
+            reverse(curr.begin(), curr.end());
+        }
+        solve(root->left, curr); 
+        solve(root->right, curr);
+        curr.pop_back();
     }
+
+    string smallestFromLeaf(TreeNode* root) {
+        string curr = "";
+        solve(root, curr);
+        return ans;
+    }
+    
+    
+    // Using BFS  TC-> O(N2)
+    // string smallestFromLeaf(TreeNode* root) {
+    //    string ans="";
+    //    queue<pair<TreeNode*,string>>q;
+    //    q.push({root,""});
+    //    while(!q.empty()){
+    //         TreeNode* frontNode=q.front().first;
+    //         string curr=q.front().second;
+    //         q.pop();
+    //         curr=char(frontNode->val+'a')+curr;
+    //         if(frontNode->left==NULL && frontNode->right==NULL){
+    //             if(ans=="" || curr<ans) ans=curr;
+    //         }
+    //         if(frontNode->left) q.push({frontNode->left,curr});
+    //         if(frontNode->right) q.push({frontNode->right,curr});
+    //    }
+    //    return ans;
+    // }
 };
