35f01e5b747ac19c866f8c008730af4f1f1c2192
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-17 11:25:09 +0530

    Time: 11 ms (43.35%), Space: 20.1 MB (82.98%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,19 +11,39 @@
  */
 class Solution {
 public:
-    string ans;
-    void solve(TreeNode* root, string curr){
-        if(!root) return;
-        curr=char(root->val+'a')+curr;
-        if(!root->left && !root->right){
-            if(ans=="" || curr<ans) ans=curr;
-            return;
-        }
-        solve(root->left,curr);
-        solve(root->right,curr);
-    }
+    // Using DFS
+    // string ans;
+    // void solve(TreeNode* root, string curr){
+    //     if(!root) return;
+    //     curr=char(root->val+'a')+curr;
+    //     if(!root->left && !root->right){
+    //         if(ans=="" || curr<ans) ans=curr;
+    //         return;
+    //     }
+    //     solve(root->left,curr);
+    //     solve(root->right,curr);
+    // }
+    // string smallestFromLeaf(TreeNode* root) {
+    //     solve(root,"");
+    //     return ans;
+    // }
+
+    // Using BFS
     string smallestFromLeaf(TreeNode* root) {
-        solve(root,"");
-        return ans;
+       string ans="";
+       queue<pair<TreeNode*,string>>q;
+       q.push({root,""});
+       while(!q.empty()){
+            TreeNode* frontNode=q.front().first;
+            string curr=q.front().second;
+            q.pop();
+            curr=char(frontNode->val+'a')+curr;
+            if(frontNode->left==NULL && frontNode->right==NULL){
+                if(ans=="" || curr<ans) ans=curr;
+            }
+            if(frontNode->left) q.push({frontNode->left,curr});
+            if(frontNode->right) q.push({frontNode->right,curr});
+       }
+       return ans;
     }
 };
