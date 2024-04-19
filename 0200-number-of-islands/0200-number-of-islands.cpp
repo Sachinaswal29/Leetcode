280b055c360c15dc9bb780727f6cffb960c0f823
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-19 10:33:30 +0530

    Time: 36 ms (23.62%), Space: 22.6 MB (29.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -10,12 +10,32 @@ public:
             dfs(grid,new_i,new_j);
         }
     }
+
+    void bfs(vector<vector<char>>& grid, int i, int j){
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        grid[i][j]='s';
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            for(auto& dir:directions){
+                int new_i=front.first+dir[0];
+                int new_j=front.second+dir[1];
+                if(new_i<0 || new_i>=grid.size() || new_j<0 || new_j>=grid[0].size() || grid[new_i][new_j]!='1') continue;
+                else{
+                    q.push({new_i,new_j});
+                    grid[new_i][new_j]='s';
+                }
+            }
+        }
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
         int count=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     count++;
                 }
             }
