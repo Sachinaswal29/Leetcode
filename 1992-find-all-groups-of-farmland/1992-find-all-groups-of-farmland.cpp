bda32cb52ecf470c2b79e76f88b4b59721a0da84
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-20 23:24:38 +0530

    Time: 355 ms (21.82%), Space: 190.1 MB (5.09%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -10,20 +10,36 @@ public:
         dfs(land,i+1,j,max_i,max_j);
         dfs(land,i,j+1,max_i,max_j);
     }
+
+    vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1}};
+    void bfs(vector<vector<int>>& land, int i, int j, int& max_i, int& max_j){
+       queue<pair<int,int>>q;
+       q.push({i,j});
+       land[i][j]=0;
+       while(!q.empty()){
+        auto it=q.front();
+        q.pop();
+        max_i=max(it.first,max_i);
+        max_j=max(it.second,max_j);
+        for(auto dir:directions){
+            int new_i=it.first+dir[0];
+            int new_j=it.second+dir[1];
+            if(new_i<0 || new_i>=land.size() || new_j<0 || new_j>=land[0].size() || land[new_i][new_j]==0) continue;
+            land[new_i][new_j]=0;
+            q.push({new_i,new_j});
+        }
+       }
+    }
+
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         vector<vector<int>>ans;
         for(int i=0;i<land.size();i++){
             for(int j=0;j<land[0].size();j++){
                 if(land[i][j]==1){
-                    vector<int>temp;
                     int max_i=i;
                     int max_j=j;
-                    temp.push_back(i);
-                    temp.push_back(j);
-                    dfs(land,i,j,max_i,max_j);
-                    temp.push_back(max_i);
-                    temp.push_back(max_j);
-                    ans.push_back(temp);
+                    bfs(land,i,j,max_i,max_j);
+                    ans.push_back({i,j,max_i,max_j});
                 }
             }
         }
