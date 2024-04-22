d4b2fefaf0d35a413ea6412bcda9e44eb14a73f5
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-22 20:24:29 +0530

    Time: 1097 ms (15.95%), Space: 219.1 MB (8.38%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    void dfs(int node, unordered_map<int,vector<int>>& um, vector<bool>& visited, int &count){
+        visited[node]=true;
+        count++;
+        for(auto currNode:um[node]){
+            if(!visited[currNode]){
+                dfs(currNode,um,visited,count);
+            }
+        }
+    }
+
+    void bfs(int node, unordered_map<int,vector<int>>& um, vector<bool>& visited, int &count){
+        queue<int>q;
+        q.push(0);
+        visited[0]=true;
+        count++;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto currNode:um[node]){
+               if(!visited[currNode]){
+                visited[currNode]=true;
+                q.push(currNode);
+                count++;
+               }
+            }
+        }
+    }
+    
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         unordered_map<int,vector<int>>um;
         for(auto edge:edges){
@@ -9,21 +37,10 @@ public:
 
         vector<bool>visited(n,false);
         for(auto res:restricted) visited[res]=true;
-        queue<int>q;
-        q.push(0);
-        int count=1;
-        visited[0]=true;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto i:um[node]){
-               if(!visited[i]){
-                visited[i]=true;
-                q.push(i);
-                count++;
-               }
-            }
-        }
+    
+        int count=0;
+        // bfs(0,um,visited,count);
+        dfs(0,um,visited,count);
         return count;
     }
 };
