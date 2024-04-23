700cbb7832e75e127ff5f88fad43d8052bfe4d18
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-04-23 10:41:36 +0530

    Time: 12 ms (75.83%), Space: 15.7 MB (68.17%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
+        // TC->   O(N2)
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=0;j<nums.size();j++){
+        //         if(i!=j && nums[i]+nums[j]==target) count++;
+        //     }
+        // }
+        // return count;
+        
+        unordered_map<string,int>um;
+        for(auto s:nums) um[s]++;
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(i!=j && nums[i]+nums[j]==target) count++;
-            }
+        for(int i=1;i<target.size();i++){
+            string first=target.substr(0,i);
+            string second=target.substr(i);
+            if(first==second) count+=um[first]*(um[second]-1);
+            else count+=um[first]*um[second];
         }
         return count;
     }
