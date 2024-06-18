7caaf68de3294537252d3495d73120653129e4cd
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-06-18 13:03:56 +0530

    Time: 60 ms (40.21%), Space: 41.2 MB (57.35%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int maxProf=0;
-        sort(worker.begin(),worker.end(),greater<int>());
-        priority_queue<pair<int, int>> pq;
+        // int maxProf=0;
+        // sort(worker.begin(),worker.end(),greater<int>());
+        // priority_queue<pair<int, int>> pq;
+        // for(int i=0;i<difficulty.size();i++){
+        //     pq.push({profit[i],difficulty[i]});
+        // }
+        // for(auto w:worker ){
+        //     while(!pq.empty() && pq.top().second > w) pq.pop();
+        //     if(!pq.empty()) maxProf+=pq.top().first;
+        // }
+        // return maxProf;
+
+        vector<pair<int,int>>vec;
         for(int i=0;i<difficulty.size();i++){
-            pq.push({profit[i],difficulty[i]});
+            vec.push_back({difficulty[i],profit[i]});
         }
-        for(auto w:worker ){
-            while(!pq.empty() && pq.top().second > w) pq.pop();
-            if(!pq.empty()) maxProf+=pq.top().first;
+        sort(vec.begin(),vec.end());
+        sort(worker.begin(),worker.end());
+
+        int j=0;
+        int totalProfit=0;
+        int maxProfit=0;
+        for(auto w:worker){
+            while(j<difficulty.size() && w>=vec[j].first){
+                maxProfit=max(maxProfit,vec[j].second);
+                j++;
+            }
+            totalProfit+=maxProfit;
         }
-        return maxProf;
+        return totalProfit;
     }
 };
