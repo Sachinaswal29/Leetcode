d99f3feea43f88538a41d6e5d3b5405a06bef0af
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-07-03 11:35:02 +0530

    Time: 74 ms (28.72%), Space: 38.8 MB (15.52%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -3,7 +3,21 @@ public:
     int minDifference(vector<int>& nums) {
         int n=nums.size();
         if(n<=4) return 0;
-        sort(nums.begin(),nums.end());
+        priority_queue<int>maxHeap;
+        priority_queue<int,vector<int>,greater<int>>minHeap;
+        for(auto x:nums){
+            maxHeap.push(x);
+            minHeap.push(x);
+            if(maxHeap.size()>4) maxHeap.pop();
+            if(minHeap.size()>4) minHeap.pop();
+        }
+        int l=3,r=n-4;
+        while(!maxHeap.empty()){
+            nums[l--]=maxHeap.top();
+            maxHeap.pop();
+            nums[r++]=minHeap.top();
+            minHeap.pop();
+        }
         return min({nums[n-4]-nums[0],nums[n-1]-nums[3],nums[n-2]-nums[2],nums[n-3]-nums[1]});
     }
 };
