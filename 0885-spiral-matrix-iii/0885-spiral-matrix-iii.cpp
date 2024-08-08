a509e1b24fd4b68c95e94e59fffb563019f8a610
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-08-08 10:48:09 +0530

    Time: 10 ms (51.13%), Space: 13.6 MB (48.36%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -2,17 +2,43 @@ class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         vector<vector<int>>ans;
-        ans.push_back({rStart,cStart});
-        int steps=0,dir=0;
-        vector<vector<int>>directions={{0,1},{1,0},{0,-1},{-1,0}};
+        // ans.push_back({rStart,cStart});
+        // int steps=0,dir=0;
+        // vector<vector<int>>directions={{0,1},{1,0},{0,-1},{-1,0}};
+        // while(ans.size()<rows*cols){
+        //     if(dir==0 || dir==2) steps++;
+        //     for(int step=0;step<steps;step++){
+        //         rStart+=directions[dir][0];
+        //         cStart+=directions[dir][1];
+        //         if(rStart<rows && rStart>=0 && cStart<cols && cStart>=0) ans.push_back({rStart,cStart});
+        //     }
+        //     dir=(dir+1)%4;
+        // }
+        
+        int steps=1;
         while(ans.size()<rows*cols){
-            if(dir==0 || dir==2) steps++;
             for(int step=0;step<steps;step++){
-                rStart+=directions[dir][0];
-                cStart+=directions[dir][1];
-                if(rStart<rows && rStart>=0 && cStart<cols && cStart>=0) ans.push_back({rStart,cStart});
+                if(rStart<rows && rStart>=0 && cStart<cols && cStart>=0) {
+                    ans.push_back({rStart,cStart});
+                }cStart++;
             }
-            dir=(dir+1)%4;
+            for(int step=0;step<steps;step++){
+                if(rStart<rows && rStart>=0 && cStart<cols && cStart>=0) {
+                    ans.push_back({rStart,cStart});
+                }rStart++;
+            }
+            steps++;
+             for(int step=0;step<steps;step++){
+                if(rStart<rows && rStart>=0 && cStart<cols && cStart>=0) {
+                    ans.push_back({rStart,cStart});
+                }cStart--;
+            }
+            for(int step=0;step<steps;step++){
+                if(rStart<rows && rStart>=0 && cStart<cols && cStart>=0) {
+                    ans.push_back({rStart,cStart});
+                }rStart--;
+            }
+            steps++;
         }
         return ans;
     }
