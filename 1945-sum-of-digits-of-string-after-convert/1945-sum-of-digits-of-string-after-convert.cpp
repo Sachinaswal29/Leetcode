1d5e4a78573b42c0e0fc56abc3c1010d2d62030e
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-03 20:37:26 +0530

    Time: 0 ms (100.00%), Space: 8 MB (77.66%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        string num="";
+        // Approach 1
+        // string num="";
+        // for(auto& ch:s){
+        //     num+=to_string(ch-'a'+1);
+        // }
+        // while(k--){
+        //     int sum=0;
+        //     for(auto& ch:num){
+        //         sum+=ch-'0';
+        //     }
+        //     num=to_string(sum);
+        //     if(num.size()==1) break;
+        // }
+        // return stoi(num);
+
+        // Approach 2
+        int sum=0;
         for(auto& ch:s){
-            num+=to_string(ch-'a'+1);
+            int val=ch-'a'+1;
+            sum+=val<10 ? val : val%10+val/10;
         }
-        while(k--){
-            int sum=0;
-            for(auto& ch:num){
-                sum+=ch-'0';
+        k--;
+        while(k-- && sum>9){
+            int temp=sum;
+            sum=0;
+            while(temp){
+                sum+=temp%10;
+                temp/=10;
             }
-            num=to_string(sum);
-            if(num.size()==1) break;
         }
-        return stoi(num);
+        return sum;
     }
 };
