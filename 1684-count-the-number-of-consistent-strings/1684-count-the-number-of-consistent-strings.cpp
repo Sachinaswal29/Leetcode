25f0a3515d6451cd8eeaa3d35c3b2873c4a05595
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-12 09:56:07 +0530

    Time: 42 ms (79.70%), Space: 33.8 MB (95.30%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        vector<int>count(26,0);
-        for(auto&x:allowed) count[x-'a']++;
+        // vector<int>count(26,0);
+        // for(auto&x:allowed) count[x-'a']++;
+        // int cnt=0;
+        // for(auto&s:words){
+        //     bool isPresent=true;
+        //     for(auto&ch:s){
+        //         if(!count[ch-'a']){
+        //             isPresent=false;
+        //             break;
+        //         }
+        //     }
+        //     if(isPresent) cnt++;
+        // }
+        // return cnt;
+
+        //Using bit Manipulation
+        int mask=0;
+        for(auto&x:allowed){
+            mask|=1<<(x-'a');
+        }
         int cnt=0;
         for(auto&s:words){
             bool isPresent=true;
             for(auto&ch:s){
-                if(!count[ch-'a']){
+                if(!((mask>>(ch-'a'))&1)){
                     isPresent=false;
                     break;
                 }
