90912f557db8a32ea0c04f0832ff377f713a9ec3
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-13 19:59:53 +0530

    Time: 127 ms (44.42%), Space: 61.6 MB (95.40%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int count(vector<int>& position, int d) {
-        int ans = 1, cur = position[0];
-        for (int i = 1; i < position.size(); ++i) {
-            if (position[i] - cur >= d) {
-                ans++;
-                cur = position[i];
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        int start=0,end=position.back()-position.front();
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int count=1,startPos=position.front();
+            for(auto& pos:position){
+                if(startPos+mid<=pos){
+                    count++;
+                    startPos=pos;
+                }
             }
+            if(count>=m){
+                ans=mid;
+                start=mid+1;
+            }
+            else end=mid-1;
         }
         return ans;
-    }
-    
-    int maxDistance(vector<int>& position, int m) {
-       sort(position.begin(), position.end());
-        int l = 0, r = position.back() - position.front();
-        while (l < r) {
-            int mid = r - (r - l) / 2;
-            if (count(position, mid) >= m)
-                l = mid;
-            else
-                r = mid - 1;
-        }
-        return l; 
     }
 };
