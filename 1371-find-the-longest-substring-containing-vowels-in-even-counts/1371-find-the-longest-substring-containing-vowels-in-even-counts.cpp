62e732fe45150db3df9cb0273175ac071bd8141e
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-15 13:35:35 +0530

    Time: 76 ms (53.49%), Space: 18.9 MB (51.86%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        vector<int>vowel(5,0);
-        unordered_map<string,int>mp;
-        string currString="00000";
-        mp[currString]=-1;
+        // vector<int>vowel(5,0);
+        // unordered_map<string,int>mp;
+        // string currString="00000";
+        // mp[currString]=-1;
+        // int maxLen=0;
+        
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='a') vowel[0]=(vowel[0]+1)%2;
+        //     if(s[i]=='e') vowel[1]=(vowel[1]+1)%2;
+        //     if(s[i]=='i') vowel[2]=(vowel[2]+1)%2;
+        //     if(s[i]=='o') vowel[3]=(vowel[3]+1)%2;
+        //     if(s[i]=='u') vowel[4]=(vowel[4]+1)%2;
+
+        //     currString="";
+        //     for(auto& ch:vowel){
+        //         currString+=to_string(ch);
+        //     }
+
+        //     if(mp.find(currString)!=mp.end()){
+        //         maxLen=max(maxLen,i-mp[currString]);
+        //     }else mp[currString]=i;
+        // }
+        // return maxLen;
+
+
+        unordered_map<int,int>mp;
+        int mask=0;
+        mp[mask]=-1;
         int maxLen=0;
         
         for(int i=0;i<s.size();i++){
-            if(s[i]=='a') vowel[0]=(vowel[0]+1)%2;
-            if(s[i]=='e') vowel[1]=(vowel[1]+1)%2;
-            if(s[i]=='i') vowel[2]=(vowel[2]+1)%2;
-            if(s[i]=='o') vowel[3]=(vowel[3]+1)%2;
-            if(s[i]=='u') vowel[4]=(vowel[4]+1)%2;
+            if(s[i]=='a') mask=mask^(1<<0);
+            if(s[i]=='e') mask=mask^(1<<1);
+            if(s[i]=='i') mask=mask^(1<<2);
+            if(s[i]=='o') mask=mask^(1<<3);
+            if(s[i]=='u') mask=mask^(1<<4);
 
-            currString="";
-            for(auto& ch:vowel){
-                currString+=to_string(ch);
-            }
-
-            if(mp.find(currString)!=mp.end()){
-                maxLen=max(maxLen,i-mp[currString]);
-            }else mp[currString]=i;
+            if(mp.find(mask)!=mp.end()){
+                maxLen=max(maxLen,i-mp[mask]);
+            }else mp[mask]=i;
         }
         return maxLen;
     }
