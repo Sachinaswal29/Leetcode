b2f272408c2e957b2e14bb36525436b01b4588a1
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-21 11:42:01 +0530

    Time: 17 ms (47.83%), Space: 14.5 MB (39.71%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
+    vector<int>ans;
+    void solve(int num, int n){
+        if(num > n) return;
+        ans.push_back(num);
+        for(int i=0;i<=9;i++){
+            solve(num*10+i,n);
+        }
+    }
+
     vector<int> lexicalOrder(int n) {
         // TC-O(NlogN)
-        vector<string>v;
-        for(int i=1;i<=n;i++) v.push_back(to_string(i));
-        sort(v.begin(),v.end());
-        vector<int>ans;
-        for(auto& num:v) ans.push_back(stoi(num));
+        // vector<string>v;
+        // for(int i=1;i<=n;i++) v.push_back(to_string(i));
+        // sort(v.begin(),v.end());
+        // vector<int>ans;
+        // for(auto& num:v) ans.push_back(stoi(num));
+        // return ans;
+
+        //TC- O(N)
+        for(int i=1;i<=9;i++){
+            solve(i,n);
+        }
         return ans;
     }
 };
