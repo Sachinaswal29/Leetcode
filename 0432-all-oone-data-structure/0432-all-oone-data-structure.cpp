7efe12f64a03c0f2df9f2e623c7ced2832d9a9fa
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-29 13:07:04 +0530

    Time: 99 ms (50.29%), Space: 55 MB (70.06%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,41 +1,70 @@
 class AllOne {
 public:
-    priority_queue<pair<int,string>>maxHeap;
-    priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>>minHeap;
+    // priority_queue<pair<int,string>>maxHeap;
+    // priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>>minHeap;
+    // unordered_map<string,int>mp;
+    // AllOne() {
+        
+    // }
+    
+    // void inc(string key) {
+    //     mp[key]++;
+    //     maxHeap.push({mp[key],key});
+    //     minHeap.push({mp[key],key});
+    // }
+    
+    // void dec(string key) {
+    //     mp[key]--;
+    //     maxHeap.push({mp[key],key});
+    //     minHeap.push({mp[key],key});
+    // }
+    
+    // string getMaxKey() {
+    //     while(maxHeap.size()){
+    //         if(maxHeap.top().first==mp[maxHeap.top().second] && mp[maxHeap.top().second]!=0){
+    //             return maxHeap.top().second;
+    //             break;
+    //         }else maxHeap.pop();
+    //     }
+    //     return "";
+    // }
+    
+    // string getMinKey() {
+    //     while(minHeap.size()){
+    //         if(minHeap.top().first==mp[minHeap.top().second] && mp[minHeap.top().second]!=0){
+    //             return minHeap.top().second;
+    //             break;
+    //         }else minHeap.pop();
+    //     }
+    //     return "";
+    // }
+    
     unordered_map<string,int>mp;
+    set<pair<int,string>>st;
     AllOne() {
         
     }
     
     void inc(string key) {
+        st.erase({mp[key],key});
         mp[key]++;
-        maxHeap.push({mp[key],key});
-        minHeap.push({mp[key],key});
+        st.insert({mp[key],key});
     }
     
     void dec(string key) {
+        st.erase({mp[key],key});
         mp[key]--;
-        maxHeap.push({mp[key],key});
-        minHeap.push({mp[key],key});
+        if(mp[key]>0) st.insert({mp[key],key});
+        else mp.erase(key);
     }
     
     string getMaxKey() {
-        while(maxHeap.size()){
-            if(maxHeap.top().first==mp[maxHeap.top().second] && mp[maxHeap.top().second]!=0){
-                return maxHeap.top().second;
-                break;
-            }else maxHeap.pop();
-        }
-        return "";
+       if(!st.empty()) return st.rbegin()->second;
+       return "";
     }
     
     string getMinKey() {
-        while(minHeap.size()){
-            if(minHeap.top().first==mp[minHeap.top().second] && mp[minHeap.top().second]!=0){
-                return minHeap.top().second;
-                break;
-            }else minHeap.pop();
-        }
+        if(!st.empty()) return st.begin()->second;
         return "";
     }
 };
