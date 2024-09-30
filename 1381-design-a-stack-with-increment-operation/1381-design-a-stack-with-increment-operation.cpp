8266c0a1e0e28e12b2bd779b4d0d18a61cc96568
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-09-30 09:45:41 +0530

    Time: 24 ms (80.39%), Space: 26 MB (29.82%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -1,41 +1,65 @@
 class CustomStack {
 public:
-    stack<int>st,st2;
-    int mSize=0;
+    // stack<int>st,st2;
+    // int mSize=0;
+    // CustomStack(int maxSize) {
+    //     mSize=maxSize;
+    // }
+    
+    // void push(int x) {
+    //     if(st.size()<mSize) st.push(x);
+    // }
+    
+    // int pop() {
+    //     if(!st.empty()) {
+    //         int el=st.top();
+    //         st.pop();
+    //         return el;
+    //     }
+    //     return -1;
+    // }
+    
+    // void increment(int k, int val) {
+    //     if(!st.empty()){
+    //         k = min(k,(int)st.size());
+    //         int n = st.size() - k;
+    //         while (n--) {
+    //             st2.push(st.top());
+    //             st.pop();
+    //         }
+    //         while(k--){
+    //             st2.push(st.top()+val);
+    //             st.pop();
+    //         }
+    //         while(!st2.empty()){
+    //             st.push(st2.top());
+    //             st2.pop();
+    //         }
+    //     }
+    // }
+
+
+    vector<int>stack;
+    int top;
+    int size;
     CustomStack(int maxSize) {
-        mSize=maxSize;
+        stack.resize(maxSize);
+        top=-1;
+        size=maxSize;
     }
     
     void push(int x) {
-        if(st.size()<mSize) st.push(x);
+        if(size-top>1) stack[++top]=x;
+        return;
     }
     
     int pop() {
-        if(!st.empty()) {
-            int el=st.top();
-            st.pop();
-            return el;
-        }
-        return -1;
+        if(top==-1) return -1;
+        return stack[top--];
     }
     
     void increment(int k, int val) {
-        if(!st.empty()){
-            k = min(k,(int)st.size());
-            int n = st.size() - k;
-            while (n--) {
-                st2.push(st.top());
-                st.pop();
-            }
-            while(k--){
-                st2.push(st.top()+val);
-                st.pop();
-            }
-            while(!st2.empty()){
-                st.push(st2.top());
-                st2.pop();
-            }
-        }
+        for(int i=0;i<k&&i<=top;i++) stack[i]+=val;
     }
 };
 
