5f24e69e198b56ce6b0a789ff3d355f3a38024eb
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-10-04 22:40:12 +0530

    Time: 54 ms (92.60%), Space: 55.8 MB (99.60%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(),skill.end());
+        // O(NlogN)
+        // sort(skill.begin(),skill.end());
+        // int left=0,right=skill.size()-1;
+        // int requiredSum=skill[left]+skill[right];
+        // long long ans=0;
+        // while(left<=right){
+        //     if(skill[left]+skill[right]==requiredSum){
+        //         ans+=(long long)skill[left++]*(long long)skill[right--];;
+        //     }else return -1;
+        // }
+        // return ans;
+
+        //O(N)
+        vector<int>count(1001,0);
+        for(auto& x:skill) count[x]++;
+
+        int j=0;
+        for(int i=0;i<1001;i++){
+            while(count[i]--) skill[j++]=i;
+        }
+        
         int left=0,right=skill.size()-1;
-        int x=skill[left]+skill[right];
+        int requiredSum=skill[left]+skill[right];
         long long ans=0;
         while(left<=right){
-            if(skill[left]+skill[right]==x){
+            if(skill[left]+skill[right]==requiredSum){
                 ans+=(long long)skill[left++]*(long long)skill[right--];;
             }else return -1;
         }
