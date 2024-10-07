b5b90749cfd16147e1e918e8c067414ef4c70f68
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-10-07 08:10:08 +0530

    Time: 9 ms (24.64%), Space: 11.4 MB (72.37%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(auto& ch:s){
+            if(!st.empty() && ch=='B' && st.top()=='A') st.pop();
+            else if(!st.empty() && ch=='D' && st.top()=='C') st.pop();
+            else st.push(ch);
+        }
+        return st.size();
+    }
+};
