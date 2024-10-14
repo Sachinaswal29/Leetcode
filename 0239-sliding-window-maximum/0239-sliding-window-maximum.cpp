88bc87d24bd459c2b7414f0062aef9bb5b9315a8
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-10-14 12:09:50 +0530

    Time: 207 ms (22.45%), Space: 152.6 MB (18.99%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
+        //Using deque
+        // deque<int>dq;
+        // vector<int>ans;
+        // for(int i=0;i<nums.size();i++){
+        //     while(!dq.empty() && nums[dq.back()]<nums[i]) dq.pop_back();
+        //     while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
+        //     dq.push_back(i);
+        //     if(i>=k-1) ans.push_back(nums[dq.front()]);
+        // }
+        // return ans;
+
+        //Using priority_queue 
+        priority_queue<pair<int,int>>pq;
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            while(!dq.empty() && nums[dq.back()]<nums[i]) dq.pop_back();
-            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
-            dq.push_back(i);
-            if(i>=k-1) ans.push_back(nums[dq.front()]);
+            while(!pq.empty() && pq.top().second<=i-k) pq.pop();
+            pq.push({nums[i],i});
+            if(i>=k-1) ans.push_back(pq.top().first);
         }
         return ans;
     }
