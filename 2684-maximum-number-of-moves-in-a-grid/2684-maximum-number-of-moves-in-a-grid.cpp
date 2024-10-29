3f415fc1e6f9fd192085f2eadbed5f641b2eef22
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-10-29 16:24:29 +0530

    Time: 13 ms (67.39%), Space: 74.6 MB (18.45%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int m,n;
-    vector<int>directions={-1,0,1};
     int dfs(vector<vector<int>>& grid, int row, int col, vector<vector<int>>& dp){
         if(dp[row][col] != -1) return dp[row][col];
-        int moves = 0;
-        for(auto& dir:directions){
-            int newRow=row+dir;
-            int newCol=col+1;
-            if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] > grid[row][col]){
-                moves = max(moves, 1 + dfs(grid, newRow, newCol, dp));
-            }
-        }
-        return dp[row][col] = moves;
+        if(col == n) return 0;
+        int right = 0,upRight = 0,downRight = 0;
+        if(col+1 < n && row-1 >= 0 && grid[row][col] < grid[row-1][col+1]) upRight = 1 + dfs(grid,row-1,col+1,dp);
+        if(col+1 < n && grid[row][col] < grid[row][col+1]) right = 1 + dfs(grid,row,col+1,dp);
+        if(row+1 < m && col+1 < n && grid[row][col] < grid[row+1][col+1]) downRight = 1 + dfs(grid,row+1,col+1,dp);
+        return dp[row][col] = max({upRight, right, downRight});
     }
     
     int maxMoves(vector<vector<int>>& grid) {
-        m = grid.size(), n = grid[0].size();
-        int moves = 0;
+        m = grid.size(),n = grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
+        int moves = 0;
         for(int i=0;i<m;i++){
             moves=max(moves,dfs(grid,i,0,dp));
         }
