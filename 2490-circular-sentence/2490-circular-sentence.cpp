9825956fef1583e2c99a6548db4ea7bc74c1a03a
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-11-02 10:24:39 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (83.91%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        stringstream ss(sentence);
-        string temp="";
-        vector<string>v;
-        while(ss>>temp) v.push_back(temp);
-        if(v.size()==1){
-            if(sentence[0]!=sentence[sentence.size()-1]) return false;
+        int n=sentence.size();  
+        if(sentence[0]!=sentence[n-1]) return 0;
+        for(int i=0;i<n;i++){
+            if(sentence[i]==' ' && sentence[i-1]!=sentence[i+1]) return 0;
         }
-        for(int i=1;i<v.size();i++){
-            string prev=v[i-1];
-            string curr=v[i];
-            if(prev[prev.size()-1]!=curr[0]) return false;
-            if(i==v.size()-1 && v[i][v[i].size()-1]!=v[0][0]) return false;
-        }
-        return true;
+        return 1;
     }
 };
